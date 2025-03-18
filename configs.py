ce46c73359edff9d210ca35111c48007dbca5b26
ak4422920 <ak4422920@gmail.com>
2025-03-18 22:46:16 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ import os
 API_ID = int(os.environ.get("API_ID", "29171167"))  # Default to a valid integer
 API_HASH = os.environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-BASE_URL = os.environ.get("BASE_URL", "tricky-dede-akad-e2b2d353.koyeb.app")
+BASE_URL = os.environ.get("BASE_URL", "https://tricky-dede-akad-e2b2d353.koyeb.app/")
 DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://akre:akre@cluster0.gtpo3kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002247400551"))  # Default to 0 if not set
 ADMINS = int(os.environ.get("ADMINS", "7251898668"))  # Default to 0 if not set
