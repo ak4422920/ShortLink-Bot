64fa28a68bf3fded5bb84ec433761a1ead7a5ea1
ak4422920 <ak4422920@gmail.com>
2025-03-18 22:35:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,16 +1,16 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", "123456"))  # Default to a valid integer
-API_HASH = os.environ.get("API_HASH", "")
+API_ID = int(os.environ.get("API_ID", "29171167"))  # Default to a valid integer
+API_HASH = os.environ.get("API_HASH", "7ea2149629e445936619f06a3c0dc716")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 BASE_URL = os.environ.get("BASE_URL", "")
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))  # Default to 0 if not set
-ADMINS = int(os.environ.get("ADMINS", "0"))  # Default to 0 if not set
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://akre:akre@cluster0.gtpo3kw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002247400551"))  # Default to 0 if not set
+ADMINS = int(os.environ.get("ADMINS", "7251898668"))  # Default to 0 if not set
 IS_FSUB = os.environ.get("IS_FSUB", "True").lower() == "true"
 
 # Convert AUTH_CHANNEL to a list of integers
-AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "")
+AUTH_CHANNELS = os.environ.get("AUTH_CHANNEL", "-1002442422204")
 AUTH_CHANNELS = [int(channel_id) for channel_id in AUTH_CHANNELS.split() if channel_id.strip()]
 
 
